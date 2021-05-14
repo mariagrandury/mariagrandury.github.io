199823c087a5d8b3edc066ec042a6a4c1ef9530d
Felix Andreas <felix.andreas95@googlemail.com>
2021-05-14 21:09:32 +0200

    Add skills page
    Co-authored-by: María Grandury <mariagrandury@gmail.com>

---
+++ src/components/SkillCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+defineProps<{
+  title: String,
+  text: String,
+  color: String,
+}>()
+</script>
+
+<template>
+  <div
+    class="bg-white p-6 border border-gray-200 rounded-md hover:shadow-md dark:border-gray-600 dark:bg-gray-800"
+  >
+    <div class="w-4rem h-4rem grid place-items-center rounded-md text-2rem" :class="color">
+      <slot></slot>
+    </div>
+    <h3 class="mt-4 mb-2 font-medium">{{ title }}</h3>
+    <p class="text-sm text-gray-700 dark:text-white">{{ text }}</p>
+  </div>
+</template>
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,24 +1,91 @@
 <template>
-  <div class="bg-blue-50 dark:bg-teal-900">
-    <div class="container mx-auto min-h-screen flex flex-col">
-      <NavBar />
-      <div
-        class="container mx-auto flex-grow grid content-center place-items-center text-3xl"
-      >
-        Here are my skills!
-      </div>
+  <Container class="bg-white dark:bg-gray-900">
+    <NavBar />
+  </Container>
+  <Container class="bg-white dark:bg-gray-900">
+    <div>
+      <h1 class="flex items-center gap-2">
+        <span class="text-4xl font-semibold">Skills</span>
+        <i-fluent-color-24-regular style="font-size: 2rem;" />
+      </h1>
+      <h2 class="py-6">I'm passionate about what I do and always learning something new!</h2>
     </div>
-  </div>
+    <div class="py-6 grid gap-6 sm:grid-cols-2 md:grid-cols-4">
+      <SkillCard
+        title="Programming"
+        text="Python: NumPy, Pandas, TensorFlow 2, Keras, Sklearn"
+        color="text-purple-600 bg-purple-50 dark:text-white dark:bg-purple-600"
+      >
+        <i-ant-design-code-outlined />
+      </SkillCard>
+      <SkillCard
+        title="Machine Learning"
+        text="ML algorithms and metrics, quality and trustworthiness"
+        color="text-blue-600 bg-blue-50 dark:text-white dark:bg-blue-600"
+      >
+        <i-ant-design-robot-outlined />
+      </SkillCard>
+      <SkillCard
+        title="Deep Learning"
+        text="DL.AI Specialization TF Professional Developer"
+        color="text-pink-600 bg-pink-50 dark:text-white dark:bg-pink-600"
+      >
+        <i-carbon-machine-learning-model />
+      </SkillCard>
+      <SkillCard
+        title="Research"
+        text="XAI, AI Robustness, AI Ethics NLP: Transformers"
+        color="text-teal-600 bg-teal-50 dark:text-white dark:bg-teal-600"
+      >
+        <i-ant-design-experiment-outlined />
+      </SkillCard>
+      <SkillCard
+        title="Math & Stats"
+        text="Double Degree in Mathematics and Physics: 5 years, 345 ECTS"
+        color="text-green-600 bg-green-50 dark:text-white dark:bg-green-600"
+      >
+        <i-ant-design-function-outlined />
+      </SkillCard>
+      <SkillCard
+        title="Software Engineering"
+        text="Experience as Software Engineer, Git, Testing, Circle-CI, Code QA"
+        color="text-indigo-600 bg-indigo-50 dark:text-white dark:bg-indigo-600"
+      >
+        <i-ant-design-code-sandbox-outlined />
+      </SkillCard>
+      <SkillCard
+        title="DevOps / MLOps"
+        text="Docker, MLflow, AWS: S3, Lambda, CloudFront, Sagemaker"
+        color="text-orange-600 bg-orange-50 dark:text-white dark:bg-orange-600"
+      >
+        <i-fluent-cloud-sync-24-regular />
+      </SkillCard>
+      <SkillCard
+        title="Technical Communication"
+        text="Data Visualization with Tableau"
+        color="text-red-600 bg-red-50 dark:text-white dark:bg-red-600"
+      >
+        <i-ant-design-pie-chart-outlined />
+      </SkillCard>
+    </div>
+    <div class="my-6 flex justify-center">
+      <a
+        href="https://github.com/mariagrandury/mariagrandury/tree/main/Certificates"
+        target="_blank"
+        class="p-6 font-medium border-2 border-dashed border-gray-200 hover:ring-4 hover:border-transparent rounded-md flex items-center gap-3"
+      >
+        <i-carbon-certificate-check class="text-1.25rem" />
+        <span>Check my certificates</span>
+        <i-carbon-certificate-check class="text-1.25rem" />
+      </a>
+    </div>
+  </Container>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from "vue";
-import NavBar from "../components/NavBar.vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Skills",
-  components: {
-    NavBar,
-  },
 });
 </script>
