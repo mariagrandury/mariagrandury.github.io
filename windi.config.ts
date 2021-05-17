1d604d961b8a56c4b159ff665318008c10d26eaf
Felix Andreas <felix.andreas95@googlemail.com>
2021-05-17 22:00:42 +0200

    Add Inter font to html header
    Co-authored-by: Maria Grandury <mariagrandury@gmail.com>

--- windi.config.ts
+++ windi.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
             accent: colors.cyan,
         },
         fontFamily: {
-            sans: ['Inter var', 'Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
+            sans: ['Inter var', 'Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
         },
     }
 })
