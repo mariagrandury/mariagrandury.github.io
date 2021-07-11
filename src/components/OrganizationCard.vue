a5bf76f16b89b6572e38b2eba6b0276e86a7be47
Felix Andreas <felix.andreas95@googlemail.com>
2021-07-11 19:58:46 +0200

    feat: add organizations to about page
    Co-authored-by: Maria Grandury <mariagrandury@gmail.com>

--- src/components/OrganizationCard.vue
+++ src/components/OrganizationCard.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 defineProps<{
-    url: string
+    img: string,
+    url?: string
 }>()
 </script>
 <template>
-    <GenericCard class="grid p-4 place-items-center">
-        <img :src="`https://mariagrandury.github.io/assets/logos/${url}`" />
+    <GenericCard class="grid">
+        <a class="contents" :href="url" target="_blank">
+            <div class="grid p-4 gap-4 place-items-center">
+                <img :src="`https://mariagrandury.github.io/assets/logos/${img}`" />
+                <slot></slot>
+            </div>
+        </a>
     </GenericCard>
 </template>
