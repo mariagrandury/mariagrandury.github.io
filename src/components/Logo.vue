a2374333b074e517fe02ad5f14df08e129b5ad28
Felix Andreas <felix.andreas95@googlemail.com>
2021-07-11 16:19:30 +0200

    feat: new logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,3 +1,10 @@
 <template>
-  <i-fluent-rating-mature-20-regular class="text-2rem" />
+  <div>
+    <!-- <span
+      class="rounded font-semibold text-lg text-black tracking-tighter p-1 dark:text-white"
+      border="2 black dark:white"
+    >M|G</span>-->
+    <!-- <span class="font-medium font-mono text-xl text-black p-1 dark:text-white">MG</span> -->
+    <span class="font-medium text-black text-xl dark:text-white" border="t-3 b-3 accent-400">MG</span>
+  </div>
 </template>
