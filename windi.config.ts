72dfe4080bf8339cb7ae6edde63537286f637633
Felix Andreas <felix.andreas95@googlemail.com>
2021-07-11 15:23:47 +0200

    feat: activate attributify mode

--- windi.config.ts
+++ windi.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'windicss/helpers'
 import colors from 'windicss/colors'
 
 export default defineConfig({
+    attributify: true,
     theme: {
         colors: {
             ...colors,
