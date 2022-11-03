0cd474215a5c57511c7b71a1ea0ab29d3eb8ebca
María Grandury <mariagrandury@gmail.com>
2022-11-04 00:33:28 +0100

    feat: center talk posters

--- src/components/CardTalk.vue
+++ src/components/CardTalk.vue
@@ -24,30 +24,21 @@ export default defineComponent({
         <div class="grid gap-3 grid-cols-[1fr,auto]">
             <div class="grid gap-3">
                 <h1 class="text-1.5rem">{{ talk }}</h1>
-                <a
-                    :href="event_link"
-                    target="_blank"
-                    class="self-start items-center hover:underline"
-                >{{ event }}</a>
+                <a :href="event_link" target="_blank" class="self-start items-center hover:underline">{{ event }}</a>
                 <div class="flex flex-wrap gap-3">
-                    <div
-                        v-for="(tag, index) in tags"
-                        :key="index"
-                        class="border rounded bg-gray-50 border-gray-100 text-sm py-0.5 px-2 select-none dark:border-black dark:bg-gray-700"
-                    >{{ tag }}</div>
+                    <div v-for="(tag, index) in tags" :key="index"
+                        class="border rounded bg-gray-50 border-gray-100 text-sm py-0.5 px-2 select-none dark:border-black dark:bg-gray-700">
+                        {{ tag }}</div>
                 </div>
             </div>
-            <a
-                v-if="recording_link"
-                :href="recording_link"
-                target="_blank"
-                class="border rounded-md flex border-gray-200 p-2 self-start items-center dark:border-black dark:bg-gray-700 hover:ring"
-            >
+            <a v-if="recording_link" :href="recording_link" target="_blank"
+                class="border rounded-md flex border-gray-200 p-2 self-start items-center dark:border-black dark:bg-gray-700 hover:ring">
                 <i-mdi-youtube style="font-size: 1.25rem;" />
             </a>
             <div class="col-start-1 col-span-full">
-                <img class="p-6" :src="image_link" :alt="talk" />
-
+                <div class="flex justify-center">
+                    <img class="p-6 max-h-80" :src="image_link" :alt="talk" />
+                </div>
                 <slot name="abstract"></slot>
             </div>
         </div>
