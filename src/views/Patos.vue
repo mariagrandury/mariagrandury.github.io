cb4e27f27e533ad0054b1454f2ba77fdc107a737
Maria Grandury <mariagrandury@gmail.com>
2023-05-02 14:50:33 +0200

    feat: new section patos

---
+++ src/views/Patos.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useHead } from '@vueuse/head'
+
+useHead({
+  title: 'Patos',
+  meta: [
+    { name: 'robots', content: 'noindex' },
+  ],
+})
+</script>
+
+<template>
+    <Container class="bg-white dark:bg-gray-900">
+        <div>
+            <h1>Patos</h1>
+            <p>Patito te quiero mucho muchísimo</p>
+        </div>
+    </Container>
+</template>
