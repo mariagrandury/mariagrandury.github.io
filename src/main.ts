7d47e2dcb422d5fb15627350707ff47a09045eb3
Maria Grandury <mariagrandury@gmail.com>
2023-05-28 12:15:04 +0200

    content: create section Patos

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import About from "./views/About.vue"
 import Contact from "./views/Contact.vue"
 import CV from "./views/CV.vue"
 import Cookies from "./views/Cookies.vue"
+import Patos from "./views/Patos.vue"
 
 export const createApp = ViteSSG(
     App,
@@ -21,7 +22,8 @@ export const createApp = ViteSSG(
             { path: '/about', component: About },
             { path: '/contact', component: Contact },
             { path: '/cv', component: CV },
-            { path: '/cookie-policy', component: Cookies }
+            { path: '/cookie-policy', component: Cookies },
+            { path: '/patos', component: Patos}
         ]
     },
     // function to have custom setups
--- src/views/Patos.vue
+++ src/views/Patos.vue
@@ -11,9 +11,40 @@ useHead({
 
 <template>
     <Container class="bg-white dark:bg-gray-900">
-        <div>
-            <h1>Patos</h1>
-            <p>Patito te quiero mucho muchísimo</p>
-        </div>
+      <NavBar />
     </Container>
+    <Container class="bg-white dark:bg-gray-900">
+      <div>
+        <h1 class="flex gap-2 items-center">
+          <div class="font-semibold tracking-tight text-4xl">Feliz Cumple Patito</div>
+          <i-noto-duck style="font-size: 2rem;" />
+          <i-twemoji-party-popper style="font-size: 2rem;" />
+        </h1>
+        <h2 class="py-6">Te quiero mucho muchísimo</h2>
+      </div>
+  
+      <div class="lg:px-84 sm:px-12">
+        <div class="grid py-6 gap-y-3 grid-cols-1">
+          <CardProject title="Cafecito"
+            link="https://somosnlp.org/hackathon">
+            <i-fluent-rocket-24-regular />
+            <template v-slot:description>
+              <div class="text-sm text-gray-700 dark:text-white">
+                Second edition of the largest open-source hackathon of NLP in
+                Spanish. This year's edition counts with +500 participants, 17 speakers, and 7 mentors. Still in progress, we'll announce the winners shortly!
+              </div>
+            </template>
+          </CardProject>
+          <CardProject title="Somos Mujeres NLP" :tags="['Women in AI', 'NLP', 'Somos NLP']"
+          link="" color="text-pink-800 bg-pink-50 dark:text-white dark:bg-pink-800">
+          <i-fluent-heart-24-regular />
+          <template v-slot:description>
+            <div class="text-sm text-gray-700 dark:text-white">
+              Organized two initiatives to promote both the work and research of women in NLP and also the projects that apply NLP to fight sexism.
+            </div>
+          </template>
+        </CardProject>  
+        </div>
+  </div>
+ </Container>
 </template>
