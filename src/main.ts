a5637aecab85fadcdd25cb69bc91975063f1bb79
María Grandury <mariagrandury@gmail.com>
2026-03-31 05:17:46 +0200

    remove extra proposals

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,8 @@ import About from "./views/About.vue"
 import Contact from "./views/Contact.vue"
 import CV from "./views/CV.vue"
 import Cookies from "./views/Cookies.vue"
-import Forbes30u30 from "./views/Forbes30u30.vue"
-import AwardProposal0 from "./views/AwardProposal0.vue"
-import AwardProposal1 from "./views/AwardProposal1.vue"
+// import Forbes30u30 from "./views/Forbes30u30.vue"
 import AwardProposal2 from "./views/AwardProposal2.vue"
-import AwardProposal3 from "./views/AwardProposal3.vue"
-import AwardProposalB from "./views/AwardProposalB.vue"
-import AwardProposalC from "./views/AwardProposalC.vue"
 
 export const createApp = ViteSSG(
     App,
@@ -32,13 +27,8 @@ export const createApp = ViteSSG(
             { path: '/contact', component: Contact },
             { path: '/cv', component: CV },
             { path: '/cookie-policy', component: Cookies },
-            { path: '/forbes30u30', component: Forbes30u30 },
-            { path: '/award/0', component: AwardProposal0 },
-            { path: '/award/1', component: AwardProposal1 },
+            // { path: '/forbes30u30', component: Forbes30u30 },
             { path: '/award/2', component: AwardProposal2 },
-            { path: '/award/3', component: AwardProposal3 },
-            { path: '/award/b', component: AwardProposalB },
-            { path: '/award/c', component: AwardProposalC },
         ]
     },
     // function to have custom setups
